8487bb8808631a1e3763d58ee1f0096b96d3fb74
unnemuta <unathi@letswhoosh.com>
2022-05-10 19:54:37 +0200

    Firebase Authentication(Sign In): Added sign in component with auth to firebase.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
-import CategoryMenu from "./components/category-menu/category-menu.component";
+// import Home from "./routes/home/home.component";
+import {Route, Routes, Outlet} from "react-router-dom";
 
 import './categories.styles.scss'
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign_in/sign_in.component";
 
 const App = () => {
-
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  return (
-    <CategoryMenu categories={categories}/>
-  );
+	return (
+		<Routes>
+			<Route path={'/'} element={<Navigation/>}>
+				<Route index element={<Home/>}/>
+				<Route path={'shop'} element={<h1>Shopping List</h1>}/>
+				<Route path={'sign_in'} element={<SignIn/>}/>
+			</Route>
+		</Routes>
+	);
 }
 
 export default App;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -34,7 +34,21 @@ export const signInWithGooglePopup = () => signInWithPopup(auth,googleProvider);
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth =  async (userAuth) => {
-	const userDocRef = doc(db,'users',userAuth.uid);
+	const userDocRef = doc(db,'users', userAuth.uid);
+
+	const userSnapshot = await getDoc(userDocRef);
+
+	if(!userSnapshot.exists()){
+		const {displayName,email} = userAuth;
+		const createdAt = new Date();
+
+		try{
+			await setDoc(userDocRef,{displayName,email,createdAt})
+		}catch (error){
+			console.log('error creating user',error.message)
+		}
+	}
+		return userDocRef;
 }
 
 
