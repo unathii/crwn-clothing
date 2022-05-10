5f3a0b8a3b4516be37df463056851f6b93f91b1c
unnemuta <unathi@letswhoosh.com>
2022-05-10 16:35:13 +0200

    Firebase Authentication(Sign In): firebase config.

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,42 @@
+import {initializeApp} from 'firebase/app';
+import {
+	getAuth,
+	signInWithRedirect,
+	signInWithPopup,
+	GoogleAuthProvider
+} from 'firebase/auth';
+
+import {
+	getFirestore,doc,getDoc,setDoc
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAg-xKs-U3RpDa-OUqVF22UlgGUDevhih4",
+	authDomain: "crwn-clothing-db-5be22.firebaseapp.com",
+	projectId: "crwn-clothing-db-5be22",
+	storageBucket: "crwn-clothing-db-5be22.appspot.com",
+	messagingSenderId: "1039007035617",
+	appId: "1:1039007035617:web:8bc98d8e7bd8ac8d4e47a0"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+	promp: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth,googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth =  async (userAuth) => {
+	const userDocRef = doc(db,'users',userAuth.uid);
+}
+
+
+
+
